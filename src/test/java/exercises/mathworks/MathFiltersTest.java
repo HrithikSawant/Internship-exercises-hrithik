f4cf2cab9762063856ca073963d30167c907f3bd
Hrithik <hrithiksawant80@gmail.com>
2022-04-12 17:15:15 +0530

    Exercise 2 refactored with Stream API

--- src/test/java/exercises/mathworks/MathFiltersTest.java
+++ src/test/java/exercises/mathworks/MathFiltersTest.java
@@ -1,109 +1,104 @@
 package exercises.mathworks;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-public class MathFiltersTest  {
+class OddCommand implements Command {
+    @Override
+    public boolean apply(Integer num) {
+        return num % 2 == 1;
+    }
+}
 
+class PrimeCommand implements Command {
+    @Override
+    public boolean apply(Integer num) {
+        return num > 1 && IntStream.rangeClosed(2, num / 2).noneMatch(i -> num % i == 0);
+    }
+}
 
+class GreaterThanX implements Command {
 
-    public static void main(String[] args) {
-        MathFilters mathFilters = new MathFilters();
+    //closure
+    private final Integer x;
+
+    public GreaterThanX(Integer x) {
+        this.x = x;
     }
 
-    public static Predicate<Integer> even() {
-        return number -> (number & 1) == 0;
+    @Override
+    public boolean apply(Integer num) {
+        return num > x;
+    }
+}
+
+class MultipleOfX implements Command {
+    //closure
+    private final Integer x;
+
+    public MultipleOfX(Integer x) {
+        this.x = x;
     }
 
-    public static Predicate<Integer> odd() {
-        return even().negate();
+    @Override
+    public boolean apply(Integer num) {
+        return num % x == 0;
     }
+}
 
-    public static Predicate<Integer> prime() {
-        return number -> {
-            if (number <= 1) { return false;}
-            int count = 2;
-            while (count * count <= number) {
-                if (number % count == 0) {return false;}
-                count++;
-            }
-            return true;
-        };
-    }
-
-    public static Predicate<Integer> getMultipleOf(int multiple) {
-        return number -> number % multiple == 0;
-    }
-
-    public static Predicate<Integer> greaterThan(int greater) {
-        return number -> number > greater;
-    }
-
-    public static Predicate<Integer> lessThan(int lessThan) {
-        return number -> number < lessThan;
-    }
-
-    public static Predicate<Integer> getOddPrimeNumbers() {
-        return odd().and(prime());
-    }
-
-    public static Predicate<Integer> getEvenAndMultipleOf(int multiple) {
-        return even().and(getMultipleOf(multiple));
-    }
-
-    public static Predicate<Integer> getOddMultipleAndGreater(int multiple, int greater) {
-        return odd().and(getMultipleOf(multiple)).and(greaterThan(greater));
-    }
+public class MathFiltersTest {
 
     @Test
     public void testGetEvenNumbers() {
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13));
-        List<Integer> evenNumbers = new ArrayList<>(Arrays.asList(2,4,6,8,10,12));
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13));
+        List<Integer> evenNumbers = new ArrayList<>(Arrays.asList(2, 4, 6, 8, 10, 12));
+        List<Command> command = new ArrayList<>(List.of(new EvenCommand()));
 
-        List<Integer> actual = MathFilters.filter(numbers,even());
+        List<Integer> actual = MyFilterEngine.filterWithAllMatches(numbers, command);
 
-        Assert.assertThat(actual, is(evenNumbers));
+        assertThat(actual, is(evenNumbers));
     }
 
     @Test
     public void testGetOddNumbers() {
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13));
-        List<Integer> oddNumbers = new ArrayList<>(Arrays.asList(1,3,5,7,9,11,13));
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13));
+        List<Integer> oddNumbers = new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9, 11, 13));
+        List<Command> command = new ArrayList<>(List.of(new OddCommand()));
 
-        List<Integer> actual = MathFilters.filter(numbers,odd());
+        List<Integer> actual = MyFilterEngine.filterWithAllMatches(numbers, command);
 
-        Assert.assertThat(actual, is(oddNumbers));
+        assertThat(actual, is(oddNumbers));
     }
 
 
     @Test
     public void testGetPrimeNumbers() {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19));
+        List<Integer> primeNumbers = new ArrayList<>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19));
+        List<Command> command = new ArrayList<>(List.of(new PrimeCommand()));
 
-        MyFilters engine = new MyFilters();
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19));
-        List<Integer> primeNumbers = new ArrayList<>(Arrays.asList(2,3,5,7,11,13,17,19));
+        List<Integer> actual = MyFilterEngine.filterWithAllMatches(numbers, command);
 
-//        List<Integer> actual = MathFilters.filter(numbers,prime());
-        List<Integer> actual = engine.filter(numbers,engine.getPreferences().get("prime"));
+        assertThat(actual, is(primeNumbers));
 
-        Assert.assertThat(actual, is(primeNumbers));
     }
 
     @Test
     public void testGetOddPrimeNumbers() {
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19));
-        List<Integer> oddPrimeNumbers = new ArrayList<>(Arrays.asList(3,5,7,11,13,17,19));
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19));
+        List<Integer> oddPrimeNumbers = new ArrayList<>(Arrays.asList(3, 5, 7, 11, 13, 17, 19));
+        List<Command> command = new ArrayList<>(List.of(new OddCommand(), new PrimeCommand()));
 
-        List<Integer> actual = MathFilters.filter(numbers,getOddPrimeNumbers());
+        List<Integer> actual = MyFilterEngine.filterWithAllMatches(numbers, command);
 
-        Assert.assertThat(actual, is(oddPrimeNumbers));
+        assertThat(actual, is(oddPrimeNumbers));
     }
 
     @Test
@@ -111,9 +106,11 @@ public class MathFiltersTest  {
         List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
         List<Integer> expected = new ArrayList<>(Arrays.asList(10, 20));
+        List<Command> command = new ArrayList<>(List.of(new EvenCommand(), new MultipleOfX(10)));
 
-        List<Integer> actual = MathFilters.filter(numbers, getEvenAndMultipleOf(5));
-        Assert.assertThat(actual, is(expected));
+        List<Integer> actual = MyFilterEngine.filterWithAllMatches(numbers, command);
+
+        assertThat(actual, is(expected));
     }
 
     @Test
@@ -121,26 +118,29 @@ public class MathFiltersTest  {
         List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
         List<Integer> expected = new ArrayList<>(List.of(15));
+        List<Command> command = new ArrayList<>(List.of(new OddCommand(), new MultipleOfX(3), new GreaterThanX(10)));
 
-        List<Integer> actual = MathFilters.filter(numbers,getOddMultipleAndGreater(3,10));
-        Assert.assertThat(actual, is(expected));
+        List<Integer> actual = MyFilterEngine.filterWithAllMatches(numbers, command);
+
+        assertThat(actual, is(expected));
     }
 
 
-
-  @Test
+    @Test
     public void testAll() {
         List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
-        List<Integer> expected_withAll = new ArrayList<>(List.of(9,15));
+        List<Integer> expected_withAll = new ArrayList<>(List.of(9, 15));
         List<Integer> expected_withAll_1 = new ArrayList<>(List.of(6, 12));
 
+        List<Command> commands1 = new ArrayList<>(List.of(new OddCommand(), new GreaterThanX(5), new MultipleOfX(3)));
+        List<Integer> actualTest1 = MyFilterEngine.filterWithAllMatches(numbers, commands1);
 
-        List<Integer> actualTest1 = MathFilters.withAllCommands(numbers,List.of(odd(),greaterThan(5),getMultipleOf(3)));
-        Assert.assertThat(actualTest1, is(expected_withAll));
+        List<Command> commands2 = new ArrayList<>(List.of(new EvenCommand(), new LessThanX(15), new MultipleOfX(3)));
+        List<Integer> actualTest2 = MyFilterEngine.filterWithAllMatches(numbers, commands2);
 
-        List<Integer> actualTest2 = MathFilters.withAllCommands(numbers,List.of(even(),lessThan(15),getMultipleOf(3)));
-        Assert.assertThat(actualTest2, is(expected_withAll_1));
+        assertThat(actualTest1, is(expected_withAll));
+        assertThat(actualTest2, is(expected_withAll_1));
     }
 
     @Test
@@ -150,11 +150,14 @@ public class MathFiltersTest  {
         List<Integer> expected_withAny = new ArrayList<>(List.of(2, 3, 5, 7, 10, 11, 13, 15, 16, 17, 18, 19, 20));
         List<Integer> expected_withAny_1 = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 9, 12, 15, 18));
 
-        List<Integer> actualTest1 = MathFilters.withAnyCommands(numbers,List.of(prime(),greaterThan(15),getMultipleOf(5)));
-        Assert.assertThat(actualTest1, is(expected_withAny));
+        List<Command> commands1 = new ArrayList<>(List.of(new PrimeCommand(), new GreaterThanX(15),
+                new MultipleOfX(5)));
+        List<Integer> actualTest1 = MyFilterEngine.filterWithAnyMatches(numbers, commands1);
+        List<Command> commands2 = new ArrayList<>(List.of(new LessThanX(6), new MultipleOfX(3)));
+        List<Integer> actualTest2 = MyFilterEngine.filterWithAnyMatches(numbers, commands2);
 
-        List<Integer> actualTest2 = MathFilters.withAnyCommands(numbers,List.of(lessThan(6),getMultipleOf(3)));
-        Assert.assertThat(actualTest2, is(expected_withAny_1));
+        assertThat(actualTest1, is(expected_withAny));
+        assertThat(actualTest2, is(expected_withAny_1));
     }
 
 }
