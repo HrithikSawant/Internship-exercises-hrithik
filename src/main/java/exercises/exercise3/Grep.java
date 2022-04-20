99893de21f5bed52e6f52b6ef254efcd6f134fae
Hrithik <hrithiksawant80@gmail.com>
2022-04-20 10:09:15 +0530

    Refactored grep and GrepTest

--- src/main/java/exercises/exercise3/Grep.java
+++ src/main/java/exercises/exercise3/Grep.java
@@ -1,66 +1,65 @@
 package exercises.exercise3;
 
-import java.io.*;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Grep {
+    public LinkedHashMap<String, List<String>> recursiveSearch(String searchWord, Path path) {
+        LinkedHashMap<String, List<String>> map = new LinkedHashMap<>();
+        try (Stream<Path> paths = Files.walk(path)) {
+            paths.filter(Files::isRegularFile)
+                    .forEach(p -> {
+                        List<String> result = searchInFile(searchWord, p);
+                        if (!result.isEmpty()) {
+                            map.put(p.toString(), result);
+                        }
+                    });
+        } catch (IOException ignore) {
+        }
+        return map;
+    }
 
     public List<String> searchInFile(String searchWord, Path filename) {
-        try (Stream<String> file = Files.lines(filename)) {
-            return search(searchWord, file.toList());//, searchOptions
-        } catch (Exception e) {
-            System.err.print(e);
-        }
-        return Collections.emptyList();
+        return search(searchWord, readFromFile(filename));
     }
 
     public List<String> searchInSubstring(String searchWord, List<String> subStrings) {
-        return search(searchWord, subStrings); //, searchOptions
+        return search(searchWord, subStrings); // searchOptions
     }
 
-    public void recursiveSearch(String searchWord, Path path) {
-        try (Stream<Path> paths = Files.walk(path)) {
-            paths.filter(Files::isRegularFile)
-                    .forEach(w -> {
-                        List<String> result = searchInFile(searchWord, w);
-                        if (!result.isEmpty()) {
-                            System.out.print(w + " : ");
-                            writeToStdOut(result);
-                        }
-                    });
+    private List<String> search(String searchStr, List<String> lines) {
+        return lines.stream()
+                .filter(s -> s.contains(searchStr))
+                .collect(Collectors.toList());
+    }
+
+    public List<String> readFromFile(Path path) {
+        try (Stream<String> stream = Files.lines(path)) {
+            return stream.collect(Collectors.toList());
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.print("File Not Found :" + e.getMessage());
         }
+        return Collections.emptyList();
     }
 
     public void writeToStdOut(List<String> result) {
         result.forEach(System.out::println);
     }
 
-    public void writeToFile(List<String> result, Path filename) {
-        try (OutputStream out = new FileOutputStream(filename.toFile());
-             Writer writer = new OutputStreamWriter(out, StandardCharsets.UTF_8)) {
-            result.forEach(str -> {
-                try {
-                    writer.write(str + "\n");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-        } catch (IOException e) {
-            System.err.print(e);
+    public void writeToFile(List<String> lines, Path path) {
+        try {
+            Files.write(path, lines, StandardCharsets.UTF_8);
+        } catch (IOException error) {
+            System.err.print("File Err" + error.getMessage());
         }
     }
 
-    private List<String> search(String searchWord, List<String> listOfLines) {
-        return listOfLines.stream()
-                .filter(s -> s.contains(searchWord))
-                .collect(Collectors.toList());
-    }
+
 }
--- src/test/java/exercises/exercise3/GrepTest.java
+++ src/test/java/exercises/exercise3/GrepTest.java
@@ -2,10 +2,12 @@ package exercises.exercise3;
 
 import org.junit.Test;
 
+import java.io.*;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Scanner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -14,49 +16,132 @@ public class GrepTest {
 
     Grep grep = new Grep();
 
-    String filePath = "src/test/java/resources/";
-
-
-    private List<String> getStrings() {
-        String search = "you";
-        return grep.searchInFile(search, Path.of(filePath + "A/lorem.txt"));
-    }
-
     @Test
     public void searchInFile() {
-        List<String> actual = getStrings();
-        List<String> expected = new ArrayList<>(List.of("During the first part of your life, you only become aware of happiness once you have lost it."));
-        assertThat(actual, is(expected));
+
+        //noMatch
+        List<String> expected_noMatch = new ArrayList<>(List.of());
+        List<String> noMatch =
+                grep.searchInFile("beautiful",
+                        Path.of("src", "test", "java", "resources", "A", "lorem.txt"));
+        assertThat(noMatch, is(expected_noMatch));
+
+        //oneMatch
+        List<String> expected_oneLineMatch = new ArrayList<>(
+                List.of("During the first part of your life."));
+        List<String> oneLineMatch =
+                grep.searchInFile("part",
+                        Path.of("src", "test", "java", "resources", "A", "lorem.txt"));
+        assertThat(oneLineMatch, is(expected_oneLineMatch));
+
+        //manyMatch
+        List<String> expected_manyMatches =
+                new ArrayList<>(List.of("During the first part of your life.",
+                        "you only become aware of happiness.", "once you have lost it."));
+        List<String> manyMatches =
+                grep.searchInFile("you",
+                        Path.of("src", "test", "java", "resources", "A", "lorem.txt"));
+        assertThat(manyMatches, is(expected_manyMatches));
+
     }
 
     @Test
-    public void printResultToStdout() {
-        List<String> actual = getStrings();
-        grep.writeToStdOut(actual);
-    }
+    public void searchInSubstring() throws FileNotFoundException {
+        InputStream StdIn = System.in;
 
-    @Test
-    public void printResultToFile() {
-        List<String> actual = getStrings();
-        grep.writeToFile(actual, Path.of(filePath + "B/output.txt"));
-        List<String> expected = new ArrayList<>(List.of("During the first part of your life, you only become aware of happiness once you have lost it."));
-        assertThat(actual, is(expected));
-    }
-
-    @Test
-    public void searchInSubstring() {
+        System.setIn(new FileInputStream("src/test/java/resources/E/test.txt"));
+        //given
         String search = "apple";
-        List<String> expected = new ArrayList<>(Arrays.asList("applesauce.", "applejacks.", "applecarts.", "appledrain.", "appleshare.",
-                "appleworks.", "appletiser.", "applecross"));
-        List<String> result = grep.searchInSubstring(search, expected);
-        grep.writeToStdOut(result);
+        List<String> expected =
+                new ArrayList<>(List.of("applesauce.", "applejacks.", "applecarts.", "appledrain.",
+                        "appleshare.", "appleworks.", "appletiser.", "applecross"));
+
+        //when
+        Scanner sc = new Scanner(System.in);
+        List<String> userInputs = new ArrayList<>();
+        while (sc.hasNext()) {
+            userInputs.add(sc.nextLine());
+        }
+        List<String> result = grep.searchInSubstring(search, userInputs);
+
+        //then
+        assertThat(expected, is(result));
+
+        System.setIn(StdIn);
+    }
+
+    @Test
+    public void printResultToStdOut() {
+        PrintStream StdOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        //given
+        List<String> lines = grep.searchInFile("you",
+                Path.of("src", "test", "java", "resources", "A", "lorem.txt"));
+
+        //when
+        grep.writeToStdOut(lines);
+
+        //then
+        assertThat("During the first part of your life." +
+                "you only become aware of happiness." +
+                "once you have lost it.", is(outputStream.toString().replaceAll("\r\n|\r|\n", "")));
+
+        System.setOut(StdOut);
     }
 
     @Test
     public void recursiveSearch() {
-        String path = "src/test/java/resources/";
-        grep.recursiveSearch("you", Path.of(path));
+        LinkedHashMap<String, List<String>> linkedHashMap = new LinkedHashMap<>();
+
+        //noMatch
+        LinkedHashMap<String, List<String>> noMatch = grep.recursiveSearch("Dog",
+                Path.of("src", "test", "java", "resources"));
+        assertThat(noMatch, is(linkedHashMap));
+
+        //one Match
+        linkedHashMap.put("src\\test\\java\\resources\\D\\C\\Test.txt",
+                List.of("This is a test file",
+                        "one can test file using testCases"));
+        LinkedHashMap<String, List<String>> oneMatch = grep.recursiveSearch("test",
+                Path.of("src", "test", "java", "resources", "D"));
+        assertThat(oneMatch, is(linkedHashMap));
+
+        //many Match
+        linkedHashMap.put("src\\test\\java\\resources\\E\\Friends.txt",
+                List.of("This is first testFile",
+                        "This is second testFile"));
+        LinkedHashMap<String, List<String>> manyMatches = grep.recursiveSearch("test",
+                Path.of("src", "test", "java", "resources"));
+        assertThat(manyMatches, is(linkedHashMap));
     }
+
+    @Test
+    public void ReadFileAndWrite() {
+
+        //given
+        List<String> lines =
+                grep.readFromFile(Path.of("src", "test", "java", "resources", "A", "lorem.txt"));
+        //when
+        grep.writeToFile(lines, Path.of("src", "test", "java", "resources", "A", "output.txt"));
+        List<String> actual =
+                grep.readFromFile(Path.of("src", "test", "java", "resources", "A", "output.txt"));
+
+        //then
+        assertThat(actual, is(lines));
+    }
+
+
+    //    @Test
+//    public void throwsForFileNotFound(){
+//        //Non Existing File
+//
+//        assertThatThrownBy(() -> grep.searchInFile("you",
+//                Path.of("src", "test", "java", "resources", "A", "no_such_file.txt")))
+//                .isInstanceOf(IOException.class)
+//                .hasMessageContaining("File Not Found");
+//    }
 
 
 }
